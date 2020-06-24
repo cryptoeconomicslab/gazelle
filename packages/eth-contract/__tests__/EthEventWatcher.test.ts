45b7806e3c165f5db69cc5d871bf6c30c8cdc400
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-06-24 23:44:55 +0900

    add testcase

--- packages/eth-contract/__tests__/EthEventWatcher.test.ts
+++ packages/eth-contract/__tests__/EthEventWatcher.test.ts
@@ -4,16 +4,24 @@
 import EventWatcher from '../src/events/EthEventWatcher'
 import { EventDb } from '@cryptoeconomicslab/contract'
 import { mocked } from 'ts-jest/utils'
+import { Bytes } from '@cryptoeconomicslab/primitives'
 jest.mock('@cryptoeconomicslab/contract')
 const undefindMock = jest.fn().mockResolvedValue(undefined)
 let specifiedToBlock = 0
+let specifiedLoggedToBlock = 0
 const funcGetLogs = async function(param: any): Promise<[]> {
   specifiedToBlock = param.toBlock
   return []
 }
+const funcSetLastLoggedBlock = async function(
+  _: Bytes,
+  blockNumber: number
+): Promise<void> {
+  specifiedLoggedToBlock = blockNumber
+}
 mocked(EventDb).mockImplementation((): any => {
   return {
-    setLastLoggedBlock: undefindMock
+    setLastLoggedBlock: funcSetLastLoggedBlock
   }
 })
 describe('EventWatcher', () => {
@@ -25,15 +33,17 @@ describe('EventWatcher', () => {
         } as any,
         kvs: {} as any,
         contractAddress: {} as any,
-        contractInterface: this.connection.interface,
+        contractInterface: {} as any,
         options: {
           interval: 0,
           approval: 3
         }
       })
       specifiedToBlock = 0
+      specifiedLoggedToBlock = 0
       await eventWatcher.poll(0, 5, () => {})
       expect(specifiedToBlock).toBe(2)
+      expect(specifiedLoggedToBlock).toBe(2)
     })
     test('When the approval option is not specified, the toBlock option is set to the current block number', async () => {
       const eventWatcher = new EventWatcher({
@@ -42,14 +52,16 @@ describe('EventWatcher', () => {
         } as any,
         kvs: {} as any,
         contractAddress: {} as any,
-        contractInterface: this.connection.interface,
+        contractInterface: {} as any,
         options: {
           interval: 0
         }
       })
       specifiedToBlock = 0
+      specifiedLoggedToBlock = 0
       await eventWatcher.poll(0, 5, () => {})
       expect(specifiedToBlock).toBe(5)
+      expect(specifiedLoggedToBlock).toBe(5)
     })
   })
 })
