7388540cec75c6b405707ee2e9166edb18537e63
tkmct <1220t.takamichi@gmail.com>
2020-07-09 02:24:12 +0900

    remove import

--- packages/plasma-light-client/src/index.ts
+++ packages/plasma-light-client/src/index.ts
@@ -1,6 +1,5 @@
 import LightClient from './LightClient'
 import UserAction, { ActionType } from './UserAction'
 
-export * from './managers'
 export default LightClient
 export { ActionType, UserAction }
