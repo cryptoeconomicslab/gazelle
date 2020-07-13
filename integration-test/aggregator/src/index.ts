1b1d4d2cbab628c043c46494b36b8de349a49d1e
Shuhei Hiya <syuhei176@gmail.com>
2020-07-13 20:55:45 +0900

    fix aggregator

--- integration-test/aggregator/src/index.ts
+++ integration-test/aggregator/src/index.ts
@@ -52,7 +52,8 @@ const instantiate = async (
       network === undefined || network === 'local'
         ? new ethers.providers.JsonRpcProvider(process.env.MAIN_CHAIN_HOST)
         : ethers.getDefaultProvider(network)
-    )
+    ),
+    contractConfig
   )
 
   const stateBucket = await kvs.bucket(Bytes.fromString('state_update'))
