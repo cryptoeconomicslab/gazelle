57d4bbde9a4473b0e94012d7a581e512d8072094
tkmct <1220t.takamichi@gmail.com>
2020-07-15 18:14:17 +0900

    Add contains method in Range.ts

--- packages/primitives/src/Range.ts
+++ packages/primitives/src/Range.ts
@@ -1,4 +1,5 @@
 import { Struct, BigNumber, Bytes } from './'
+import JSBI from 'jsbi'
 
 export default class Range {
   constructor(readonly start: BigNumber, readonly end: BigNumber) {}
@@ -48,5 +49,16 @@ export default class Range {
 
   public equals(range: Range): boolean {
     return this.start.equals(range.start) && this.end.equals(range.end)
+  }
+
+  /**
+   * checks if this range contains given range
+   * @param range range to be checked if its contained
+   */
+  public contains(range: Range): boolean {
+    return (
+      JSBI.greaterThanOrEqual(range.start.data, this.start.data) &&
+      JSBI.lessThanOrEqual(range.end.data, this.end.data)
+    )
   }
 }
