6b3717d088ba1c942bff1d3e8ec277bb408d4313
daiki.sekiguchi <daiki.skgc44@gmail.com>
2020-08-13 01:16:52 +0900

    change subscribeExitFinalized argument StateUpdate to Exit

--- packages/plasma-light-client/src/LightClient.ts
+++ packages/plasma-light-client/src/LightClient.ts
@@ -482,7 +482,7 @@ export default class LightClient {
     this.ee.on(EmitterEvent.TRANSFER_COMPLETE, handler)
   }
 
-  public subscribeExitFinalized(handler: (su: StateUpdate) => void) {
+  public subscribeExitFinalized(handler: (exit: Exit) => void) {
     this.ee.on(EmitterEvent.EXIT_FINALIZED, handler)
   }
 
@@ -500,7 +500,7 @@ export default class LightClient {
     this.ee.off(EmitterEvent.TRANSFER_COMPLETE, handler)
   }
 
-  public unsubscribeExitFinalized(handler: (su: StateUpdate) => void) {
+  public unsubscribeExitFinalized(handler: (exit: Exit) => void) {
     this.ee.off(EmitterEvent.EXIT_FINALIZED, handler)
   }
 }
