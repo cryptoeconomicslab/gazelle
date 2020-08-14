0eb3160ddd8fcc2d62e0b31d3bdd5bc6d946a5e1
Shuhei Hiya <syuhei176@gmail.com>
2020-08-14 22:25:46 +0900

    optimize sync root

--- packages/plasma-light-client/src/usecase/StateSyncer.ts
+++ packages/plasma-light-client/src/usecase/StateSyncer.ts
@@ -152,8 +152,8 @@ export class StateSyncer {
       // if aggregator latest state doesn't have client state, client should check spending proof
       // clear verified state updates
       await this.syncTransfers()
-      //  sync root hashes
-      await this.syncRootUntil(blockNumber)
+      //  sync root hashes from `synced+1` to `blockNumber`
+      await this.syncRootUntil(synced.increment(), blockNumber)
 
       const verifyStateUpdate = async (su: StateUpdate, retryTimes = 5) => {
         try {
@@ -205,16 +205,22 @@ export class StateSyncer {
     }
   }
 
-  public async syncRootUntil(blockNumber: BigNumber) {
-    let synced = blockNumber
-    while (JSBI.greaterThan(synced.data, JSBI.BigInt(0))) {
-      const root = await this.commitmentContract.getRoot(synced)
-      await this.storeRoot(synced, root)
-      synced = BigNumber.from(JSBI.subtract(synced.data, JSBI.BigInt(1)))
+  /**
+   * sync Merkle Root from `from` number to `to` number.
+   * @param from
+   * @param to
+   */
+  private async syncRootUntil(from: BigNumber, to: BigNumber) {
+    let b = to
+    const synced = JSBI.subtract(from.data, JSBI.BigInt(1))
+    while (JSBI.greaterThan(b.data, synced)) {
+      const root = await this.commitmentContract.getRoot(b)
+      await this.storeRoot(b, root)
+      b = BigNumber.from(JSBI.subtract(b.data, JSBI.BigInt(1)))
     }
   }
 
-  public async storeRoot(blockNumber: BigNumber, root: FixedBytes) {
+  private async storeRoot(blockNumber: BigNumber, root: FixedBytes) {
     const { coder } = ovmContext
     const rootHint = Hint.createRootHint(
       blockNumber,
